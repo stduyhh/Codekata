fce3ea5cb72e9ca09b04b4b5239d49af31ab3f88
stduyhh <ghls1252@gmail.com>
2025-05-05 12:54:53 +0900

    Hap commit

---
+++ Hap/Hap.java
@@ -0,0 +1,8 @@
+package Algorithm.comparison.Hap;
+
+public class Hap {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
---
+++ Hap/Main.java
@@ -0,0 +1,10 @@
+package Algorithm.comparison.Hap;
+
+public class Main {
+    public static void main(String[] args) {
+        Hap solution = new Hap();
+
+        int s = solution.solution(1,102);
+        System.out.println(s);
+    }
+}
