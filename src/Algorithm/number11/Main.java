ca980f5929c9f4130d1fedf63035ccee59f57f5d
stduyhh <ghls1252@gmail.com>
2025-05-09 09:40:44 +0900

    number11

---
+++ src/Algorithm/number11/Main.java
@@ -0,0 +1,10 @@
+package Algorithm.number11;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String  x = solution.solution(667);
+        System.out.println(x);
+    }
+}
---
+++ src/Algorithm/number11/Solution.java
@@ -0,0 +1,13 @@
+package Algorithm.number11;
+
+public class Solution {
+    public String solution(int num) {
+        String answer;
+
+        if(num % 2 == 0){
+            return answer = "Even";
+        } else {
+            return answer = "Odd";
+        }
+    }
+}
